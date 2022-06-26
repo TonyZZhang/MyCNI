8ca0216e5acc69f7d57c2c27344ead7291c2b857
zhanghaizhou <Zhanghaizhou@hollysys.net>
2022-06-26 22:00:54 +0800

    读取配置文件

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"encoding/json"
+	"github.com/containernetworking/cni/pkg/types"
 	"github.com/containernetworking/cni/pkg/version"
 	bv "github.com/containernetworking/plugins/pkg/utils/buildversion"
 	"mycni/skel"
 	utils "mycni/util"
 )
+
+type PluginConf struct {
+	types.NetConf
+	// 这里可以自由定义自己的 plugin 中配置了的参数然后自由处理
+	Bridge string `json:"bridge"`
+	Subnet string `json:"subnet"`
+}
 
 func cmdAdd(args *skel.CmdArgs) error {
 	utils.WriteLog("进入到 cmdAdd")
@@ -16,6 +25,14 @@ func cmdAdd(args *skel.CmdArgs) error {
 		"Args: ", args.Args,
 		"Path: ", args.Path,
 		"StdinData: ", string(args.StdinData))
+
+	pluginConfig := &PluginConf{}
+	err := json.Unmarshal(args.StdinData, pluginConfig)
+	if err != nil {
+		utils.WriteLog(err.Error())
+		return err
+	}
+
 	return nil
 }
 
