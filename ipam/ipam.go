00fe08497f2d8fca8122f0485d9b062752c7404f
zhanghaizhou <Zhanghaizhou@hollysys.net>
2022-06-28 11:16:55 +0800

    增加ipam测试

--- ipam/ipam.go
+++ ipam/ipam.go
@@ -1,6 +1,10 @@
 package ipam
 
-import "strings"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 
 type Ipam struct {
@@ -16,8 +20,26 @@ func (i *Ipam) SetSegmentMask()  {
 	i.SegmentMask = strs[0]
 }
 
-func (i *Ipam) SetNodeNetSegment(nodeName string) {
-	
+func (i *Ipam) GenerateNodeNetSegmentPool() string{
+	fmt.Println(i.SegmentMask)
+	ipSeg := strings.Split(i.SegmentMask, ".")
+	var temp int
+	var nodeIpSeg, ipPool string
+	for k, v := range ipSeg {
+		if v == "0" {
+			temp = k
+			break
+		}
+	}
+	for i:=0; i< 256; i++ {
+		ipSeg[temp] = strconv.Itoa(i)
+		nodeIpSeg = strings.Join(ipSeg, ".")
+		ipPool += nodeIpSeg
+		ipPool += ";"
+	}
+	return ipPool
 }
+
+func (i *Ipam) SetNodeNetSegment(nodeName string) {
 
 }
