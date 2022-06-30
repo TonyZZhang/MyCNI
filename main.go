2e051983962599d557c09f48efde43c7b6b8dc41
zhanghaizhou <1078760300@qq.com>
2022-06-30 20:22:04 +0800

    Setup a IP address

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/containernetworking/cni/pkg/types"
 	"github.com/containernetworking/cni/pkg/version"
 	bv "github.com/containernetworking/plugins/pkg/utils/buildversion"
+	"github.com/vishvananda/netlink"
 	"mycni/netlinktool"
 	"mycni/skel"
 	utils "mycni/util"
+	"net"
 )
 
 type PluginConf struct {
@@ -39,12 +42,35 @@ func cmdAdd(args *skel.CmdArgs) error {
 
 	//每一个Node分配同一个子网下的不同网段
 	//pluginConfig.Subnet
-
+	IP := ""
 	//实现同一Node之间Pod网络互通
-	err = netlinktool.CreateBridgeAndSetupVeth(pluginConfig.Bridge, args.Netns, args.IfName)
+	netns, err := ns.GetNS(args.Netns)
 	if err != nil {
 		return err
 	}
+
+	err = netlinktool.CreateBridgeAndSetupVeth(pluginConfig.Bridge, netns, args.IfName)
+	if err != nil {
+		return err
+	}
+
+	//Setup a IP address
+	err = netns.Do(func(hostNS ns.NetNS) error {
+		// create the veth pair in the container and move host end into host netns
+
+		link, err := netlink.LinkByName(args.IfName)
+		if err != nil {
+			return err
+		}
+		ipv4Addr, ipv4Net, err := net.ParseCIDR(IP)
+		addr := &netlink.Addr{IPNet: ipv4Net, Label: ""}
+		ipv4Net.IP = ipv4Addr
+		if err = netlink.AddrAdd(link, addr); err != nil {
+			return fmt.Errorf("failed to add IP addr %v to %q: %v", ipv4Net, args.IfName, err)
+		}
+		return nil
+	})
+
 
 	return nil
 }
